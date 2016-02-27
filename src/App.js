7457c045357749fda78dac283b03a57ad52697b8
Maurice <mcsmorenberg@gmail.com>
2016-02-27 20:16:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+          <div className="container">
+            <nav className="main">
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/games">Projects</Link></li>
+                <li><Link to="/about">About</Link></li>
+              </ul>
+            </nav>
+            {this.props.children}
+          </div>
+        );
+    }
+}
+
+export default App;
