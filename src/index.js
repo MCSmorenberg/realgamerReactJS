00c3fa89719a3c27947218f47ca31773a4d9fb90
Maurice <mcsmorenberg@gmail.com>
2016-02-27 20:36:26 +0100

    Basic file structure and routing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
 import App from './App';
+import ProjectList from './GameList';
+import Project from './Game';
+import PageNotFound from './PageNotFound';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={GameList} />
+      <Route path="/game/:gameId" component={Game}/>
+      <Route path="*" component={PageNotFound}/>
+    </Route>
+  </Router>
+), document.getElementById('root'));
