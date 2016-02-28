d336e91c3c82a640302bcb6047c150400ffa4bbf
Maurice <mcsmorenberg@gmail.com>
2016-02-28 18:57:53 +0100

    jquery imported and get json funtionality

--- src/GameList.js
+++ src/GameList.js
@@ -1,17 +1,42 @@
 import React from 'react';
+import jQuery from 'jquery';
 import { Link } from 'react-router';
 
 class GameList extends React.Component {
-  render() {
-    let gameId = 1;
+  constructor() {
+    super();
 
+     this.state = {
+       games: []
+     };
+   }
+
+   reloadGames(event) {
+     let component = this;
+
+     jQuery.getJSON("https://realgamer.herokuapp.com/games", function(data) {
+       console.log(data);
+
+       component.setState({
+         games: data.games
+       });
+     });
+   }
+
+   componentDidMount() {
+     console.log("componentDidMount");
+     this.reloadGames();
+   }
+
+    render() {
       return(
         <div>
           <h1>Games!</h1>
-          <p>
-          <Link to={`/game/${gameId}`}>Game 1</Link>
-        </p>
-        {this.props.children}
+            <table>
+              <tbody>
+                {this.state.games.map(this.renderGame.bind(this))
+             </tbody>
+           </table>
         </div>
       );
     }
